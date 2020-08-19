312fa6d5511b9e3889ff087986e3e8fa24378810
Alain <63358696+AldoLaClasse@users.noreply.github.com>
2020-08-19 10:54:55 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,10 @@ version = "1.0.0-SNAPSHOT"
 publishing {
     repositories {
         maven(url = "https://mymavenrepo.com/repo/ah37AFHxnt3Fln1mwTvi/")
+        credentials {
+                username myMavenRepoUser
+                password myMavenRepoPassword
+            }
     }
     publications {
         create<MavenPublication>("MaskModel") {
