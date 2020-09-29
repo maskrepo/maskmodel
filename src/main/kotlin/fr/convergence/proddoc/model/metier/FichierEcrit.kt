959bc343b080e1bfe7fd5627b65bf2b5eced634b
AldoLaClasse <alain.meynier@gmail.com>
2020-09-29 11:28:16 +0200

    amélioration de l'objet métier "FichierEcrit" qui devient "StokageFichier"

--- src/main/kotlin/fr/convergence/proddoc/model/metier/FichierEcrit.kt
+++ src/main/kotlin/fr/convergence/proddoc/model/metier/FichierEcrit.kt
@@ -3,19 +3,27 @@ package fr.convergence.proddoc.model.metier
 import kotlinx.serialization.Serializable
 
 @Serializable
-class FichierEcrit(
-    /**
-     * URL absolue du fichier sur le FileSystem
-     */
-    val fichierURLAbs :String,
-    /**
-     * URL relative du fichier
-     */
-    val fichierURLRel :String,
-    /**
-     * identifiant métier unique du fichier
-     */
-    val idMetierFichier :String
+class StockageFichier(
+        /**
+         * identifiant métier unique du fichier
+         */
+        val idMetierFichier: String,
+        /**
+         * URL absolue du fichier sur le FileSystem
+         */
+        val fichierURLAbs: String?,
+        /**
+         * URL relative du fichier
+         */
+        val fichierURLRel: String?,
+        /**
+         * paramètres de requête http éventuels
+         */
+        val Parametres: Map<String, String> ?,
+        /**
+         * eventuel chemin complet sur le file system
+         */
+        val fichierSileSystemURL :String?
 )
 
 @Serializable
