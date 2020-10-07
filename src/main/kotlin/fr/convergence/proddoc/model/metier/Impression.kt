9594291a5900ff6676faac7f03eb1666fa6ae31d
AldoLaClasse <alain.meynier@gmail.com>
2020-10-07 17:35:21 +0200

    nouveaux objets métier pour les demandes d'impression

---
+++ src/main/kotlin/fr/convergence/proddoc/model/metier/Impression.kt
@@ -0,0 +1,38 @@
+package fr.convergence.proddoc.model.metier
+
+import kotlinx.serialization.Serializable
+
+/**
+ * pour faire bonne impression !
+ */
+@Serializable
+class DemandeImpression (
+    /**
+     * L'URL absolue pour accèder au fichier via un stream
+     */
+    val urlFichierAImprimer: String,
+    /**
+     * L'URL de callback pour confirmer l'état de l'impression
+     */
+    val urlCallback : String?
+)
+
+@Serializable
+class RetourImpression (
+        /**
+         * message détaillant le retour
+         */
+        val messageRetour :String
+)
+
+@Serializable
+class RetourImpressionMyGreffe (
+        /**
+         * L'URL absolue du fichier qui devrait être imprimé
+         */
+        val urlFichierAImprimer: String,
+        /**
+         * message détaillant le retour
+         */
+        val messageRetour :String
+)
